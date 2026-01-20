67847b70cfeb8b2af4c39304f4c94701e2ea029b
ESchwenker <Eschwenker24@outlook.com>
2026-01-20 10:10:21 -0500

    ugh

--- index.js
+++ index.js
@@ -4,18 +4,20 @@
 // http://img.omdbapi.com/?apikey=[a5e7ab33]&
 
 const moviesWrapper = document.querySelector('.movies');
+const searchName = document.querySelector('.searchName');
 
 
 function searchChange(event) {
   renderMovies(event.target.value)
+  searchName.innerHTML = event.target.value
 }
 
 async function renderMovies(searchTerm) {
-  const response = await fetch("https://www.omdbapi.com/?apikey=a5e7ab33&s=${searchTerm}")
+  const response = await fetch(`https://www.omdbapi.com/?apikey=a5e7ab33&s=${searchTerm}`)
   const data = await response.json();
   const moviesArr = data.Search;
   console.log(moviesArr);
-  moviesWrapper.innerHTML = moviesArr.map((movie) => {
+  moviesWrapper.innerHTML = moviesArr.slice(0, 9).map((movie) => {
     return `<div class="movie">
               <img class="movie__poster" src="${movie.Poster}">
               <h3 class="movie__title">${movie.Title}</h3>

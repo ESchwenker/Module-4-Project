9d86976be8a1930f6ec8e199e940ada72acadf57
ESchwenker <Eschwenker24@outlook.com>
2026-01-20 15:44:50 -0500

    fin?

--- index.js
+++ index.js
@@ -3,30 +3,56 @@
 // poster
 // http://img.omdbapi.com/?apikey=[a5e7ab33]&
 
+
+// GRABBING ELEMENTS FROM HTML
 const moviesWrapper = document.querySelector('.movies');
 const searchName = document.querySelector('.searchName');
 
+// GLOBAL MOVIES VARIABLE
+let currentMovies = []
 
+
+// HANDLING THE SEARCH
 function searchChange(event) {
   renderMovies(event.target.value)
   searchName.innerHTML = event.target.value
 }
 
+// RENDERING- CALLING API
 async function renderMovies(searchTerm) {
   const response = await fetch(`https://www.omdbapi.com/?apikey=a5e7ab33&s=${searchTerm}`)
   const data = await response.json();
-  const moviesArr = data.Search;
-  console.log(moviesArr);
-  moviesWrapper.innerHTML = moviesArr.slice(0, 9).map((movie) => {
+  currentMovies = data.Search;
+  displayMovies(currentMovies);
+}
+
+// DISPLAYING
+function displayMovies(movieList) {
+    moviesWrapper.innerHTML = movieList.slice(0, 9).map((movie) => {
     return `<div class="movie">
               <img class="movie__poster" src="${movie.Poster}">
               <h3 class="movie__title">${movie.Title}</h3>
               <h3 class="movie__year">${movie.Year}</h3>
             </div>`
-  }).join('')
-
+  }).join('');
 }
 
+// SORTING
+function sortChange(event) {
+  const sortOption = event.target.value
+
+  let sortedMovies = [...currentMovies]
+
+  if (sortOption === "newest") {
+    sortedMovies.sort((a, b) => b.Year - a.Year)
+  } else if (sortOption === "oldest") {
+    sortedMovies.sort((a, b) => a.Year - b.Year)
+  } else if (sortOption === "name") {
+    sortedMovies.sort((a, b) => a.Title.toLowerCase().localeCompare(b.Title.toLowerCase()))
+  }
+
+  displayMovies(sortedMovies);
+}
 
 
 
